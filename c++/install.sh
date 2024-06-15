99f9afe648642b8074d071901d68fae59d86b64e
Klbgr <qiuantoine@gmail.com>
2024-06-15 23:45:04 +0200

    fixed install.sh script

--- c++/install.sh
+++ c++/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-./install_lccv.sh
-./install_ncnn.sh
-./install_onnx.sh
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+$SCRIPT_DIR/install_lccv.sh
+$SCRIPT_DIR/install_ncnn.sh
+$SCRIPT_DIR/install_onnx.sh
